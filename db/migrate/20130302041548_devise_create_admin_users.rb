86a00f1cba77999c20464b78393f843ab3877b4b
Patrick Joly <rock.patrick.joly@gmail.com>
2013-03-02 00:34:19 -0500

    added I18 gem

--- db/migrate/20130302041548_devise_create_admin_users.rb
+++ db/migrate/20130302041548_devise_create_admin_users.rb
@@ -41,7 +41,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
     end
 
     # Create a default user
-    AdminUser.create!(:email => 'katiag', :password => 'Pipolechien99', :password_confirmation => 'Pipolechien99')
+    AdminUser.create!(:email => 'katiag@stellabijoux.com', :password => 'Pipolechien99', :password_confirmation => 'Pipolechien99')
 
     add_index :admin_users, :email,                :unique => true
     add_index :admin_users, :reset_password_token, :unique => true
