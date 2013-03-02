8c1dd4ba6e89c9b14c773f1019bceb72d94f0d88
Patrick Joly <rock.patrick.joly@gmail.com>
2013-03-01 22:38:24 -0500

    fix db script

--- db/migrate/20130302023229_fix_category_id_to_product.rb
+++ db/migrate/20130302023229_fix_category_id_to_product.rb
@@ -1,6 +1,6 @@
 class FixCategoryIdToProduct < ActiveRecord::Migration
   def change
-     remove_column :products, :categoryId, :integer
+
     add_column :products, :category_id, :integer
   end
 
