af892fee5f05a39a2a64ca4b49b3f5195985eb36
Patrick Joly <rock.patrick.joly@gmail.com>
2013-05-07 14:12:32 -0400

    fixes for local

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     title ||= I18n.t(theTitle)
     direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
     selected = params[:selected_category]
-    locale = params[:locale]
+    locale = I18n.locale == I18n.default_locale ? 'fr' : 'en'
     link_to title, :locale => locale , :sort => column, :direction => direction, :selected_category => selected
   end
 end
