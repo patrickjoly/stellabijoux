f821b73754b83161e157cdbfa1f04d79faad6b1b
Patrick Joly <patrick_joly@hotmail.com>
2013-06-07 11:38:12 -0400

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,11 @@
-
 CarrierWave.configure do |config|
   config.root = Rails.root.join('tmp') # adding these...
   config.cache_dir = 'carrierwave' # ...two lines
 
   config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIWBD37GIQMJC7WKQ',                        # required
-    :aws_secret_access_key  => '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',                     # required
+    :provider               => 'xx',                        # required
+    :aws_access_key_id      => 'xx',                        # required
+    :aws_secret_access_key  => 'xx',                     # required
 
   }
   config.fog_directory  = 'amazonBucket'                             # required
